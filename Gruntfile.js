39204a71c77c4b830ae23b95088ac220f6dab3e3
LAPTOP-57J009DG\piotr <kubus-o_2@tlen.pl>
2017-07-05 15:25:55 +0200

    Zmodyfikowałem Gruntfile.js do zadania_7_4

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,52 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-  	sass: {
-  		options: {
-  			sourceMap: true
-  		},
-  		dist: {
-  			files: {
-  				'css/main.css': 'sass/main.sass'
-  			}
-  		}
-  	},
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/main.css': 'sass/main.sass'
+                }
+            }
+        },
 
-  	imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		}
-  	},
-	watch: {
-	    scripts: {
-	        files: ['sass/*.sass'],
-	        tasks: ['sass'],
- 	         options: {
-  	          spawn: false,
-       		 },
- 	   }
-	},
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
         browserSync: {
-	    bsFiles: {
- 	       src : 'sass/*.sass'
-	    },
- 	   options: {
-		proxy: "http://localhost/~Zadanie_8_5/"
-    }
-}
-  });
+            bsFiles: {
+                src : 'sass/*.sass'
+            },
+            options: {
+                proxy: "http://localhost/~Zadanie_8_5/"
+            }
+        }
+    });
   // Load the plugins tasks 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
   
-  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync']);
+    grunt.registerTask('default', ['sass', 'imagemin', 'browserSync']);
 };
